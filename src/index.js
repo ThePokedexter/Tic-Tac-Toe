a7095ff7559530b98a4a7959ed1543b15b215eef
Karen Guo <karenguo@live.com>
2018-08-24 10:03:41 -0700

    Add redux

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
 import Game from './components/Game';
 
+import './index.css';
+
+import * as reducers from './store/reducers';
+const store = createStore(combineReducers(reducers), applyMiddleware());
+
 ReactDOM.render(
-  <Game />,
+  <Provider store={store}>
+    <Game />
+  </Provider>,
   document.getElementById('root')
 );
