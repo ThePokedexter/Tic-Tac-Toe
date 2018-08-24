e91803afb3e5adb7dd6d9a4bf19116784514070b
Karen Guo <karenguo@live.com>
2018-08-24 11:43:42 -0700

    Add redux-devtools

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
 import Game from './components/Game';
+import { devToolsEnhancer } from 'redux-devtools-extension';
 
 import './index.css';
 
 import * as reducers from './store/reducers';
-const store = createStore(combineReducers(reducers), applyMiddleware());
+const store = createStore(combineReducers(reducers), devToolsEnhancer());
 
 ReactDOM.render(
   <Provider store={store}>
